540768e4c383ba9539eaaa38d36ab4f1051b8a20
Andrew Miller <dmiller309@gmail.com>
2018-08-14 07:53:46 -0700

    Add logs to show where the Trace/breakpoint trap is happening.
    $ electron-webgl
    Gtk-Message: Failed to load module "canberra-gtk-module"
    Gtk-Message: Failed to load module "canberra-gtk-module"
    main.js 1
    main.js 2
    main.js 3
    main.js 4
    main.js 5
    main.js 6
    main.js 7
    main.js 8
    main.js 9
    createWindow 1
    Trace/breakpoint trap (core dumped)

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 "use strict"
 
+console.log("main.js 1")
 const { app, BrowserWindow, ipcMain } = require('electron')
+console.log("main.js 2")
 const url = require('url')
 
 let win
 
+console.log("main.js 3")
 function createWindow () {
+  console.log("createWindow 1")
   win = new BrowserWindow({
     width: 1280,
     height: 720,
@@ -17,6 +21,7 @@ function createWindow () {
       devTools: true
     }
   })
+  console.log("createWindow 1")
 
   win.openDevTools()
 
@@ -27,25 +32,31 @@ function createWindow () {
   })
 }
 
+console.log("main.js 4")
 app.commandLine.appendSwitch('ignore-gpu-blacklist', 'true')
 
+console.log("main.js 5")
 app.on('ready', createWindow)
 
+console.log("main.js 6")
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
+console.log("main.js 7")
 process.on('uncaughtException', function (err) {
   console.error('uncaughtException')
   console.error(err)
 })
 
+console.log("main.js 8")
 app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
 })
 
+console.log("main.js 9")
 
